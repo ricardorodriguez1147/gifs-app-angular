008d12bb205efe1b8ecd7a1f361751dadba06944
ricardorodriguez1147 <ricardorod1147@gmail.com>
2025-01-28 23:52:53 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  giphyApiKey: NEXT_PUBLIC_GIPHY_API_KEY //'HyqW8VwnPRnDaRyOf2KyBRrITvNjwCWR'// Lee desde la variable de entorno
+  giphyApiKey: window['NEXT_PUBLIC_GIPHY_API_KEY'] || 'default_api_key'  //'HyqW8VwnPRnDaRyOf2KyBRrITvNjwCWR'// Lee desde la variable de entorno
 };
